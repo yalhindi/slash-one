2829c9869d2232223035f39509db20f2cadb23ba
Yazan Alhindi <y.alhindi@outlook.com>
2026-03-18 20:57:22 +0100

    style(auth): change layout background color to a dark color for the "membrane aura" to show correctly at step 4 of creating an account.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#020617] via-[#082f49] to-[#020617] p-4">
+        <div className="min-h-screen flex items-center justify-center bg-slate-950">
 
             {children}
 
